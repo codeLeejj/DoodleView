4ddd52ffad4d33052b684c072de29b38f66f411a
annis <annis.junjielee@gmail.com>
2020-12-21 21:02:49 +0800

    添加Bitmap 工具类

---
+++ boodleLib/src/main/java/com/lee/boodlelib/utils/BitmapUtil.java
@@ -0,0 +1,56 @@
+package com.lee.boodlelib.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * @program: DoodleView
+ * @description: Bitmap 工具类
+ * @author: lee
+ * @create: 2020-12-21 21:01
+ */
+public class BitmapUtil {
+    public static String bitmap2Base64(Bitmap bitmap) {
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            if (bitmap != null) {
+                baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+                baos.flush();
+                baos.close();
+
+                byte[] bitmapBytes = baos.toByteArray();
+                result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.flush();
+                    baos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * base64转为bitmap
+     *
+     * @param base64Data
+     * @return
+     */
+    public static Bitmap base642Bitmap(String base64Data) {
+        byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+}
